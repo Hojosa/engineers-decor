c954d229b8acfbe72cef1570095e41f180f490a8
Hojosa <mccloud.fox21@gmail.com>
2026-07-18 02:23:30 +0200

    remap removed slag blocks to IE blocks

---
+++ src/main/java/wile/engineersdecor/EdEvents.java
@@ -0,0 +1,41 @@
+package wile.engineersdecor;
+
+import blusunrize.immersiveengineering.common.register.IEBlocks;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.MissingMappingsEvent;
+
+@Mod.EventBusSubscriber(modid = ModEngineersDecor.MODID)
+public class EdEvents {
+
+	@SubscribeEvent
+    public static void onMissingMappings(MissingMappingsEvent event) {
+    	// For blocks
+    	for (MissingMappingsEvent.Mapping<Block> mapping : event.getMappings(ForgeRegistries.Keys.BLOCKS, ModEngineersDecor.MODID)) {
+    	    if(mapping.getKey().getPath().equals("slag_brick_block")) {
+    	    	mapping.remap(IEBlocks.StoneDecoration.SLAG_BRICK.get());
+    	    }
+    	    if(mapping.getKey().getPath().equals("slag_brick_slab")) {
+    	    	mapping.remap(IEBlocks.TO_SLAB.get(IEBlocks.StoneDecoration.SLAG_BRICK.getId()).get());
+    	    }
+    	    if(mapping.getKey().getPath().equals("slag_brick_stairs")) {
+    	    	mapping.remap(IEBlocks.TO_STAIRS.get(IEBlocks.StoneDecoration.SLAG_BRICK.getId()).get());
+    	    }
+    	}
+    	// For items
+    	for (MissingMappingsEvent.Mapping<Item> mapping : event.getMappings(ForgeRegistries.Keys.ITEMS, ModEngineersDecor.MODID)) {
+    		if(mapping.getKey().getPath().equals("slag_brick_block")) {
+    	    	mapping.remap(IEBlocks.StoneDecoration.SLAG_BRICK.asItem());
+    	    }
+    		if(mapping.getKey().getPath().equals("slag_brick_slab")) {
+    	    	mapping.remap(IEBlocks.TO_SLAB.get(IEBlocks.StoneDecoration.SLAG_BRICK.getId()).asItem());
+    	    }
+    	    if(mapping.getKey().getPath().equals("slag_brick_stairs")) {
+    	    	mapping.remap(IEBlocks.TO_STAIRS.get(IEBlocks.StoneDecoration.SLAG_BRICK.getId()).asItem());
+    	    }
+    	}
+	}
+}
